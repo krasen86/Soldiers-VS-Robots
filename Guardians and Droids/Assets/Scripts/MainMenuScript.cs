5362a9dc4f478b0f61f9ec3cbe855f0af929ed4e
krasen86 <krasenparvanov@gmail.com>
2020-06-18 15:59:36 +0200

    Create main page
    Add basic main page menu with settings options for sound controll and rankings as well as an exit button.

---
+++ Guardians and Droids/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void StartNewGame() {
+        SceneManager.LoadScene("CreatePlayer");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Has quit Game");
+    }
+}
